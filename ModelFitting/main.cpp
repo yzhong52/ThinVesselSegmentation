5bbedb6403b6e47caea8cd9c1aef1599ead926d0
Yuchen <yzhong.cs@gmail.com>
2014-02-05 11:56:47 -0500

    Create Project Model Fitting

---
+++ ModelFitting/main.cpp
@@ -0,0 +1,15 @@
+// ModelFitting.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+// This project is build after VesselNess. 
+// Some of the building blocks (Data3D, Visualization) are borrowed from VesselNess. 
+
+int main(int argc, char* argv[])
+{
+	// TODO: Fix this function.
+	
+	return 0;
+}
+
