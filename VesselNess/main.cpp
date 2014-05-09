6155a2dcd1cb2bf77a30dea6c64c644bad913059
Yuchen <yzhong.cs@gmail.com>
2014-05-09 10:17:41 -0400

    Update main.cpp

--- VesselNess/main.cpp
+++ VesselNess/main.cpp
@@ -27,30 +27,33 @@ namespace sample_code{
 		// create output folders if it does not exist
 		CreateDirectory(L"../temp", NULL);
 
-		bool flag = false;
-
-		// Parameters for Vesselness
+		// Sigma: Parameters for Vesselness
+		// [sigma_from, sigma_to]: the potential size rang of the vessels
+		// sigma_step: precision of computation
 		float sigma_from = 1.0f;
 		float sigma_to = 8.10f;
 		float sigma_step = 0.5f;
-		// Parameters for vesselness
+		// Parameters for vesselness, please refer to Frangi's papaer 
+		// or this [blog](http://yzhong.co/?p=351)
 		float alpha = 1.0e-1f;	
 		float beta  = 5.0e0f;
 		float gamma = 3.5e5f;
 
 		// laoding data
 		Data3D<short> im_short;
-		bool falg = im_short.load( "../data/" + dataname + ".data" );
+		bool flag = im_short.load( "../data/" + dataname + ".data" );
+		if(!flag) return 0;
 		
-		if(!falg) return 0;
-		
+		// Compute Vesselness
 		Data3D<Vesselness_Sig> vn_sig; 
 		VesselDetector::compute_vesselness( im_short, vn_sig, 
 			sigma_from, sigma_to, sigma_step,
 			alpha, beta, gamma );
+			
+		// Saving Data
 		vn_sig.save( "../temp/" + dataname + ".vn_sig" );
 
-		// If you want to visulize the data
+		// If you want to visulize the data using Maximum-Intensity Projection
 		if( isDisplay ) {
 			viewer.addObject( vn_sig,  GLViewer::Volumn::MIP );
 			viewer.addDiretionObject( vn_sig );
