aee74193e3a3ff918693e4a6f9cdfa25c8d2acec
yzhong52 <yzhong52@ubuntu.ubuntu-domain>
2014-05-26 18:32:22 -0400

    created project install
    copy files to specific folder

---
+++ install/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+
+//  ANSI [colour codes](http://en.wikipedia.org/wiki/ANSI_escape_code)
+const string RED = "\033[1;31m";
+const string GREEN = "\033[1;32m";
+const string BLACK = "\x1b[0;49m";
+
+int main()
+{
+
+    vector<std::pair<string, string> > files2copy
+    {
+        std::pair<string, string>("../SparseMatrixCV/bin/Release/libSparseMatrixCV.a",
+                                  "../libs/libSparseMatrixCV.a")
+    };
+
+    std::ifstream  src;
+    std::ofstream  dst;
+    for( unsigned i=0; i<files2copy.size(); i++ )
+    {
+        const string& from = files2copy[i].first;
+        const string& to = files2copy[i].second;
+
+        src.open( from );
+        dst.open( to );
+        if( src.fail() ) {
+            cout << RED << " [FAIL   ] " << BLACK;
+            cout << "Unable to open file '" << from << "'" << endl;
+        } else {
+            dst << src.rdbuf();
+            cout << GREEN << " [SUCCESS] " << BLACK;
+            cout << "File copied from '" << from  << "'";
+            cout << " to '" << to << "'" << endl;
+        }
+    }
+}
