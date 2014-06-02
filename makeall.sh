b81be375f9b6bfcd0bfaf3ff4ada703ff89268f2
yzhong52 <yzhong.cs@gmail.com>
2014-06-02 12:16:31 -0400

    update makefiles from c++11 to c++0x

--- makeall.sh
+++ makeall.sh
@@ -5,17 +5,41 @@
 #
 ####################################
 
+echo "\n"
+echo "####################################"
+echo "# Compiling Vesselness"
+echo "####################################"
+echo "\n"
+
 cd ./Vesselness
 make clean
 make
+
+echo "\n"
+echo "####################################"
+echo "# Compiling SparseMatrix"
+echo "####################################"
+echo "\n"
 
 cd ../SparseMatrix
 make clean
 make
 
+echo "\n"
+echo "####################################"
+echo "# Compiling SparseMatrixCV"
+echo "####################################"
+echo "\n"
+
 cd ../SparseMatrixCV
 make clean
 make
+
+echo "\n"
+echo "####################################"
+echo "# Compiling ModelFitting"
+echo "####################################"
+echo "\n"
 
 cd ../ModelFitting
 make clean
