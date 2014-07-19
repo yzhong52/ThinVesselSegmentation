fc050bc4c03db1fcd077f5bf8cf3020f8752c1f2
yzhong52 <yzhong.cs@gmail.com>
2014-07-19 12:31:59 -0400

    [Sharcnet] Updata setting files

--- setup-sharcnet-saw.sh
+++ setup-sharcnet-saw.sh
@@ -1,19 +1,21 @@
-module unload intel/12.1.3
+# This is for testing on Sharcnet
 
+# Change compiler
+module unload intel/12.1.3
 module load gcc/4.8.2
 
-module load opencv/2.4.9
-
+# Python
 module load python/3.2.2
 
+
+# OpenCV
+module load opencv/2.4.9
 export PKG_CONFIG_PATH=/opt/sharcnet/opencv/2.4.9/lib/pkgconfig
-
 pkg-config --modversion opencv
-
 pkg-config --cflags opencv
-
 pkg-config --libs opencv
 
+# Show all modules
 module list
 
 ####################################
