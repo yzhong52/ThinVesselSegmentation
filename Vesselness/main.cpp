5859b16d0a9072bfe1d3c152fb05b894bf423467
yzhong52 <yzhong.cs@gmail.com>
2014-07-21 17:24:35 -0400

    Tunning Parameters for ROI16 (new cropped)

--- Vesselness/main.cpp
+++ Vesselness/main.cpp
@@ -13,11 +13,6 @@
 // Linux header: provide system call functions such as sleep()
 #include <unistd.h>
 
-
-#define INPUT_DIR "../data/"
-#define OUTPUT_DIR "../temp/"
-
-
 using namespace std;
 using namespace cv;
 
@@ -34,9 +29,10 @@ int centreline( bool isDisplay, std::string dataname = "data15" );
 
 int main(void)
 {
-    Mat temp = Mat(200, 200, CV_8UC3);
-    // cv::imshow( "", temp );
+    Mat temp = Mat(10, 10, CV_8UC3);
+    cv::imshow( "", temp );
 
+    /*
     Image3D<short> im_data;
     im_data.load( "../temp/vessel3d_rd_sp.data" );
     im_data.setROI( );
@@ -46,10 +42,10 @@ int main(void)
     viewer.display(600, 400, 1);
 
     return 0;
+    /**/
 
-
-    sample_code::vesselness(false, "data15", 2800 );
-    sample_code::centreline(true,  "data15");
+    sample_code::vesselness(false, "../temp/roi16", 2800 );
+    sample_code::centreline(true,  "../temp/roi16");
     return 0;
 }
 
@@ -61,18 +57,18 @@ int sample_code::vesselness( bool isDisplay, string dataname, short threshold )
     // Sigma: Parameters for Vesselness
     // [sigma_from, sigma_to]: the potential size rang of the vessels
     // sigma_step: precision of computation
-    float sigma_from = 1.0f;
-    float sigma_to   = 8.10f;
-    float sigma_step = 0.3f;
+    float sigma_from = 0.65f;
+    float sigma_to   = 10.10f;
+    float sigma_step = 0.27f;
     // Parameters for vesselness, please refer to Frangi's papaer
     // or this [blog](http://yzhong.co/?p=351)
-    float alpha = 1.0e-1f;
-    float beta  = 5.0e0f;
-    float gamma = 3.5e5f;
+    float alpha = 0.3e0f;
+    float beta  = 6.8e0f;
+    float gamma = 4.5e5f; // Increase: small vessel disappear
 
-    // laoding data
+    // Loading original data
     Image3D<short> im_short_orig;
-    bool flag = im_short_orig.load( INPUT_DIR + dataname + ".data" );
+    bool flag = im_short_orig.load( dataname + ".data" );
     if( !flag ) return 0;
 
     Data3D<short> im_short;
@@ -83,7 +79,7 @@ int sample_code::vesselness( bool isDisplay, string dataname, short threshold )
     VesselDetector::compute_vesselness( im_short, vn_sig,
                                         sigma_from, sigma_to, sigma_step,
                                         alpha, beta, gamma );
-    vn_sig.save( OUTPUT_DIR + dataname + ".vn_sig" );
+    vn_sig.save( dataname + ".vn_sig" );
 
     // If you want to visulize the data using Maximum-Intensity Projection
     if( isDisplay )
@@ -92,7 +88,7 @@ int sample_code::vesselness( bool isDisplay, string dataname, short threshold )
         viewer.addObject( im_short,  GLViewer::Volumn::MIP );
         viewer.addObject( vn_sig,  GLViewer::Volumn::MIP );
         viewer.addDiretionObject( vn_sig );
-        viewer.display(600, 400, 4);
+        viewer.display(800, 600, 4);
     }
 
     return 0;
@@ -101,23 +97,30 @@ int sample_code::vesselness( bool isDisplay, string dataname, short threshold )
 int sample_code::centreline( bool isDisplay, string dataname )
 {
     // load vesselness data
-    Data3D<Vesselness_Sig> vn_sig( OUTPUT_DIR + dataname + ".vn_sig" );
+    Data3D<Vesselness_Sig> vn_sig( dataname + ".vn_sig" );
 
     // non-maximum suppression
     Data3D<Vesselness_Sig> vn_sig_nms;
     IP::non_max_suppress( vn_sig, vn_sig_nms );
-    vn_sig_nms.save( OUTPUT_DIR + dataname + ".nms.vn_sig" );
+    vn_sig_nms.save( dataname + ".nms.vn_sig" );
 
     // edge tracing
     Data3D<Vesselness_Sig> vn_sig_et;
-    IP::edge_tracing( vn_sig_nms, vn_sig_et, 0.45f, 0.05f );
-    vn_sig_et.save( OUTPUT_DIR + dataname + ".et.vn_sig" );
+    IP::edge_tracing( vn_sig_nms, vn_sig_et, 0.05f, 0.01f );
+    vn_sig_et.save( dataname + ".et.vn_sig" );
+
+    // Loading original data
+    Data3D<short> im_short_orig;
+    bool flag = im_short_orig.load( dataname + ".data" );
+    if( !flag ) return 0;
 
     if( isDisplay )
     {
+        viewer.addObject( im_short_orig );
+        viewer.addObject( vn_sig );
         viewer.addObject( vn_sig_et,  GLViewer::Volumn::MIP );
         viewer.addDiretionObject( vn_sig_et );
-        viewer.display(600, 400, 2);
+        viewer.display(800, 600, 4);
     }
     return 0;
 }
